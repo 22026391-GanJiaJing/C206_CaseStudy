0230ba3d983247150ba850f6f13557dab917ad59
22026391 <22026391@LAPTOP-AKO8QBM6>
2023-08-06 20:02:19 +0800

    jia jing

--- C206_CaseStudy/src/Tastybites.java
+++ C206_CaseStudy/src/Tastybites.java
@@ -23,169 +23,189 @@ public class Tastybites {
 		String name="";
 		String password="";
 
-		while (option != 3 && loginprocess==false) {
 
-			Tastybites.Mainmenu();
-			option = Helper.readInt("Enter an option > ");
+		while(loginprocess==false) {
+			while (option != 3 && loginprocess==false) {
 
-			if (option == 1) {
-				// Create new Account
-				CreateAccount(AccountList);
-
-
-			} else if (option == 2) {
-				// Login
-				name =Helper.readString("Username: ");
-				password=Helper.readString("Password: ");
-
-				for (Account a:AccountList) {
-					if(a.getName().equalsIgnoreCase(name) && a.getPassword().equalsIgnoreCase(password)) {
-						loginprocess=true;
-						System.out.println("Login successful");
-						used=a;
-					}
-				}
-				if (loginprocess==false) {
-					System.out.println("Wrong Username or Password");
-				}
-			} else if (option == 3  ) {
-				System.out.println("Closing System");
-				System.exit(0);
-			} else {
-				System.out.println("Invalid option");}}
-		
-		//for staff
-		if(used.getRole().equalsIgnoreCase("staff")){
-			option = 0;
-			while (option != 3 ) {
-
-				Tastybites.Staffmenu();
+				Tastybites.Mainmenu();
 				option = Helper.readInt("Enter an option > ");
 
 				if (option == 1) {
-					// View all Accounts
-					ViewAllAccount(AccountList);
+					// Create new Account
+					CreateAccount(AccountList);
+
 
 				} else if (option == 2) {
-					// Add new stall && display
-					String stallname =Helper.readString("Name: ");
+					// Login
+					name =Helper.readString("Username: ");
+					password=Helper.readString("Password: ");
 
-					StallList.add(new Stall(stallname));
-
-				}else if (option == 3) {
-					// Delete Stalls && display
-					ViewAllStalls(StallList);
-
-					String SelectStall=Helper.readString("Name of Stall You Want to Delete: ");
-
-					boolean found=false;
-
-					for (Stall a : StallList) {
-						if(a.getName().equalsIgnoreCase(SelectStall)) {
-							StallList.remove(a);
-							found=true;
-							System.out.println("Stall deleted");
+					for (Account a:AccountList) {
+						if(a.getName().equalsIgnoreCase(name) && a.getPassword().equalsIgnoreCase(password)) {
+							loginprocess=true;
+							System.out.println("Login successful");
+							used=a;
 						}
 					}
-					if (found==false) {
-						System.out.println("Stall not found");
+					if (loginprocess==false) {
+						System.out.println("Wrong Username or Password");
 					}
-
-
-				}
-				else if (option == 4) {
-					ViewAllStalls(StallList);
-					String stallchoose=Helper.readString("Choose Stall to Edit: " );
-					
-					
-
-				}
-				else if (option == 5) {
-					// view queues choose to delete queue (completed order)
-					System.out.println(String.format("%-20s%s", "NAME"));
-					for (Account a : QueueList) {
-						System.out.println(String.format("%-20s%s", a.getName()));
-					}
-
-				}
-				else if (option == 6) {
-					// view all feedback
-					for (Stall a : StallList) {
-						System.out.println(String.format("%-20s%s", a.getName()));
-					}
-
-				}
-
-				else if (option == 7 ) {
+				} else if (option == 3  ) {
 					System.out.println("Closing System");
+					System.exit(0);
 				} else {
 					System.out.println("Invalid option");}}
-		}
-		else if(used.getRole().equalsIgnoreCase("customer")) {
-			//for customer
-			option = 0;
-			while (option != 3 ) {
 
-				Tastybites.Customermenu();
-				option = Helper.readInt("Enter an option > ");
+			//for staff
+			if(used.getRole().equalsIgnoreCase("staff")){
+				option = 0;
+				while (option != 3 && loginprocess==true) {
 
-				if (option == 1) {
-					// Delete Account
+					Tastybites.Staffmenu();
+					option = Helper.readInt("Enter an option > ");
 
-					Helper.line(100, "-");
-					char choice=Helper.readChar("Are you sure to delete Account?(Y/N) > ");
-					Helper.line(100, "-");
-					if(choice=='Y') {
+					if (option == 1) {
+						// View all Accounts
+						ViewAllAccount(AccountList);
 
+					} else if (option == 2) {
+						// Add new stall && display
+						String stallname =Helper.readString("Name: ");
+
+						StallList.add(new Stall(stallname));
+
+					}else if (option == 3) {
+						// Delete Stalls && display
+						ViewAllStalls(StallList);
+
+						String SelectStall=Helper.readString("Name of Stall You Want to Delete: ");
+
+						boolean found=false;
 
 						for (Stall a : StallList) {
-							if(a.getName().equalsIgnoreCase(name)) {
+							if(a.getName().equalsIgnoreCase(SelectStall)) {
 								StallList.remove(a);
+								found=true;
 								System.out.println("Stall deleted");
 							}
 						}
+						if (found==false) {
+							System.out.println("Stall not found");
+						}
+
+
 					}
-
-				} else if (option == 2) {
-					ViewAllStalls(StallList);
-
-				} else if (option == 3) {
-					//Add to cart
-					ViewCart(Cart);
+					else if (option == 4) {
+						ViewAllStalls(StallList);
+						String stallchoose=Helper.readString("Choose Stall to Edit: " );
 
 
 
-				}
-				else if (option == 4) {
-					//Delete from cart
-					ViewCart(Cart);
+					}
+					else if (option == 5) {
+						// view queues choose to delete queue (completed order)
+						System.out.println(String.format("%-20s%s", "NAME"));
+						for (Account a : QueueList) {
+							System.out.println(String.format("%-20s%s", a.getName()));
+						}
+
+					}
+					else if (option == 6) {
+						// view all feedback
+						for (Stall a : StallList) {
+							System.out.println(String.format("%-20s%s", a.getName()));
+						}
+
+					}
+					else if (option == 7) {
+						// Delete Account
+
+						Helper.line(100, "-");
+						char choice=Helper.readChar("Are you sure to delete Account?(Y/N) > ");
+						Helper.line(100, "-");
+						if(choice=='Y') {
+
+
+							for (Account a : AccountList) {
+								if(a.getName().equalsIgnoreCase(name)) {
+									AccountList.remove(a);
+									System.out.println("Stall deleted");
+									loginprocess=false;
+								}
+							
+						
+
+					else if (option == 8 ) {
+						System.out.println("Closing System");
+					} else {
+						System.out.println("Invalid option");}}
+			}
+			else if(used.getRole().equalsIgnoreCase("customer")) {
+				//for customer
+				option = 0;
+				while (option != 3 && loginprocess==true) {
+
+					Tastybites.Customermenu();
+					option = Helper.readInt("Enter an option > ");
+
+					if (option == 1) {
+						// Delete Account
+
+						Helper.line(100, "-");
+						choice=Helper.readChar("Are you sure to delete Account?(Y/N) > ");
+						Helper.line(100, "-");
+						if(choice=='Y') {
+
+
+							for (Account a : AccountList) {
+								if(a.getName().equalsIgnoreCase(name)) {
+									AccountList.remove(a);
+									System.out.println("Stall deleted");
+									loginprocess=false;
+								}
+							}
+						}
+
+					} else if (option == 2) {
+						ViewAllStalls(StallList);
+
+					} else if (option == 3) {
+						//Add to cart
+						ViewCart(Cart);
 
 
 
-				}
-				else if (option == 5) {
-					//View cart 
-					ViewCart(Cart);
+					}
+					else if (option == 4) {
+						//Delete from cart
+						ViewCart(Cart);
 
 
 
-				}
-				else if (option == 6) {
-					//make payment (add to queue)
-					ViewCart(Cart);
+					}
+					else if (option == 5) {
+						//View cart 
+						ViewCart(Cart);
 
 
 
-				}else if (option == 7) {
-					System.out.println("Closing System");
-				} else {
-					System.out.println("Invalid option");}}
+					}
+					else if (option == 6) {
+						//make payment (add to queue)
+						ViewCart(Cart);
 
 
+
+					}else if (option == 7) {
+						System.out.println("Closing System");
+					} else {
+						System.out.println("Invalid option");}}
+
+
+
+			}
 
 		}
-
-
 	}
 
 	/**
@@ -195,7 +215,7 @@ public class Tastybites {
 		Helper.line(100, "-");
 		System.out.println(String.format("%-20s%s", "NAME"));
 		for (MenuItem a:Cart) {
-			
+
 			System.out.println(String.format("%-20s%s", a.getName()));
 		}
 		Helper.line(100, "-");
@@ -255,7 +275,8 @@ public class Tastybites {
 		System.out.println("4. Edit Menu");
 		System.out.println("5. Edit Queue");
 		System.out.println("6. View Feedback");
-		System.out.println("7. Quit");
+		System.out.println("7. Delete my Account");
+		System.out.println("8. Quit");
 		Helper.line(80, "-");
 
 	}
